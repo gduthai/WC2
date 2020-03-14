963070d5d1bfd836f3afc1ceaeaa9c7e35f81bd2
gduthai <44573787+gduthai@users.noreply.github.com>
2020-03-14 19:23:25 +0800

    Update WC2/WC2.c

--- WC2/WC2.c
+++ WC2/WC2.c
@@ -45,7 +45,7 @@ int linenumber(char* filename) {//统计行数
 	FILE* fp = NULL;
 	int c, lc = 0;
 	int line = 0;
-	fp = fopen(filename, "r");
+	fp = fopen(filename, "r");   
 	if (wordnumber(filename) == 0)
 		return 0;
 	while ((c = fgetc(fp)) != EOF) {
